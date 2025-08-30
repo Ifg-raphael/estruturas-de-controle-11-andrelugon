fe41de1606dc0faf02de5281cab57c0994ebb3bf
andrelugon <andreluiz.lugon@gmail.com>
2025-08-30 18:35:56 -0300

    Resolução do exercício classifica_triangulo.py

--- classifica_triangulo.py
+++ classifica_triangulo.py
@@ -1,1 +1,25 @@
-# Sua solução aqui
+#Programa recebe três valores, verifica se esses valores formam um triângulo e classifica-o como equilátero, isósceles e escaleno:
+
+## Entrada de dados: o usuário fornece os valores a,b,c 
+a = int(input("Forneça o valor de a: "))
+b = int(input("Forneça o valor de b: "))
+c = int(input("Forneça o valor de c: "))
+
+#Estrutura condicional: Verifica se os valores informados formam ou não um triângulo
+if (a < b + c) and (b < a + c) and (c < b + a):
+
+    #Estrutura condicional: Verifica o tipo de triângulo formado
+    if (a == b):
+        if (b == c): 
+            print ('Triangulo equilátero') #Saída de dados: informa que o triângulo é do tipo equilátero
+        elif (b != c):
+            print ('Triângulo isósceles') #Saída de dados: informa que o triângulo é do tipo isósceles
+        else:
+            print ('Triângulo escaleno') #Saída de dados: informa que o triângulo é do tipo escaleno
+    elif (a != b):
+        if (b == c):
+            print ('Triângulo Isósceles') #Saída de dados: informa que o triângulo é do tipo isósceles
+        else:
+            print ('Triângulo escaleno') #Saída de dados: informa que o triângulo é do tipo escaleno
+else:
+    print('Não forma triângulo') #Saída de dados: informa que os dados informados não forma um triângulo
